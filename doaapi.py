919f6b905d234406ffb7b00c8c2622e284d1bab6
k-rite <44746687+k-rite@users.noreply.github.com>
2022-08-02 12:01:57 +0530

    v

--- doaapi.py
+++ doaapi.py
@@ -2,28 +2,27 @@ from flask import Flask, jsonify
 app= Flask(__name__)
 @app.route('/v1/<string:name>/bubble', methods=['GET'])
 def game(name):
-    msg = ""
     list= []
-    lista = int(name.split('-'))
-    for a in lista:
-        a= int(a)
-        if a not in list:
-            list.append(int(lista))
-
-    swap,iterations =0,0
-    while list != sorted(list): #checks weather list is sorted or not
+    iterations =[]
+    lista = name.split('-')
+    c= sorted(list)
+    for i in range (0,len(lista)):
+        list.append(int(lista[i]))
+    swap = 0
+    def sort(a):
+        global swap
+        try:# iterations
+            if list[a]>list[a+1]: # b a c
+                list.insert(a+2,list[a]) # b a b c
+                list.pop(a) # a b c
+                swap = swap+1
+        except:
+            iterations.append(list)
+            pass
+    while list != c:
         for a in range(len(list)):
-            try:# iterations
-                if list[a]>list[a+1]: # b a c
-                    list.insert(a+2,list[a]) # b a b c
-                    list.pop(a) # a b c
-                    swap = swap+1
-            except:
-                iterations=iterations+1
-                msg = f'{msg} \n,  iteration {iterations} {list}'
-                pass
-            else:
-                a=f'{msg}, \n Sorted list {list}, Number of swaps:{swap}, Number of iterations:{iterations}'
-                return jsonify({"message":a})
-
+            sort(a) #checks weather list is sorted or not
+    else:
+        a=f'Sorted list {iterations} , Number of swaps:{swap}, Number of iterations:{len(iterations)}'
+    return jsonify({"message":a})
 app.run(port=8080)
---
+++ java8.java
@@ -0,0 +1,28 @@
+// // use void to return empty value, if method returning something use return
+// class Method_Demo {
+//     public static void main(String[] args) {
+//         int a = 20;
+//         int b = 10;
+//         Method_Demo ob = new Method_Demo();
+//         ob.add(a, b);
+//     }
+//     public void add(int a, int b) {
+//         int c = a + b;
+//         System.out.println("addition=" + c);
+//     }
+// }
+
+// use void to return empty value, if method returning something use return
+class Method_Demo {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        Method_Demo ob = new Method_Demo();
+        int c=ob.add(a, b);
+        ob.add(a, b);
+        System.out.println("addition"+c);
+    }
+    public int add(int a, int b) {
+        int c = a + b;
+    return c;    }
+}
