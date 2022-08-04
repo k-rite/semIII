74b6ab3fe3ad1542af350b647b0a766e5c7ab280
k-rite <44746687+k-rite@users.noreply.github.com>
2022-08-04 09:32:35 +0530

    f

---
+++ java9.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Arthmatic
+{
+    int a,b,c;
+    public void add()
+    {
+        c=a+b;
+    }
+    public void printdata()
+    {
+        System.out.println("Addition is"+c);
+    }
+    public void getdata()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        a=sc.nextInt();
+        System.out.println("Enter a number:");
+        b=sc.nextInt();
+        sc.close();
+    }
+}
+    
+class Arthmaticdemo
+{
+    public static void main(String[] args) {
+        Arthmatic ob = new Arthmatic();
+        ob.getdata();
+        ob.add();
+        ob.printdata();
+    }
+}
