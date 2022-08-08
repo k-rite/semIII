21974c78b06d7b2969b209bd71ac91835d317c80
k-rite <44746687+k-rite@users.noreply.github.com>
2022-08-08 09:32:17 +0530

    d

--- djanggo/django_projl/welcome/views.py
+++ djanggo/django_projl/welcome/views.py
@@ -5,9 +5,15 @@ def greet(request):
     html = "<html><body><h1>Welcome to my 1st project</h1></body></html>"
     return HttpResponse(html)
 # Create your views here.
+# def index(request):
+#     template = loader.get_template('myfirst.html')
+#     return HttpResponse(template.render())
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().value()
+    output = ""
+    for x in mymembers:
+        output += x['firstname'] 
+    return HttpResponse(output)
 
 def testing(request):
     template=loader.get_template('template.html')
