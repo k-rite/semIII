cfd20a0600dfedeac044fb663abaaf167ffb1494
k-rite <44746687+k-rite@users.noreply.github.com>
2022-08-08 09:02:12 +0530

    rf

--- doaapi.py
+++ doaapi.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 app= Flask(__name__)
 @app.route('/v1/<string:name>/bubble', methods=['GET'])
-def game(name):
+def bubble(name):
     list= []
     iterations =[]
     lista = name.split('-')
@@ -25,4 +25,28 @@ def game(name):
     else:
         a=f'Sorted list {iterations} , Number of swaps:{swap}, Number of iterations:{len(iterations)}'
     return jsonify({"message":a})
-app.run(port=8080)
+@app.route('/v1/<string:name>/selection', methods=['GET'])
+def selection(name):
+    list=[]
+    a=name.split('-')
+    for i in range (0,len(a)):
+        list.append(int(a[i]))
+    solution=sorted(list)
+   # swap = int(0)
+    msg = ''
+    def sort(a):
+        global msg
+        global swap
+         #to overwite swap variable in def()
+        if list[a] != solution[a]: #checking if elements in ascending order
+            swap_index = list.index(solution[a])
+            list[a],list[swap_index] = list[swap_index],list[a] #swapping elements
+            #swap = swap+1 #counting number of swaps
+            msg = msg + f'Swap:{swap} \n{list}'
+    while list != solution: #checks weather list is sorted or not
+        #global swap
+        for a in range(len(list)):
+            sort(a)
+    else:
+        return jsonify({msg:f"Sorted list {list}, Number of swaps:swap"}) #list is sorted :)
+app.run(port=8080,debug=True)
---
+++ java10.java
@@ -0,0 +1,20 @@
+class PassArray {
+    public int sumNumbers(int nums[]) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n[] = new int[args.length];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = Integer.parseInt(args[i]);
+
+        }
+        PassArray ob = new PassArray();
+        int s = ob.sumNumbers(n);
+        System.out.println("Sum of number" + s);
+    }
+}
