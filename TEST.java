45bcacf2702c9e5accfbb03f031c9a35a424a4e8
k-rite <44746687+k-rite@users.noreply.github.com>
2022-09-13 11:36:53 +0530

    hvbjn

---
+++ TEST.java
@@ -0,0 +1,19 @@
+class A {
+    public static void main(String args[]) {
+    char initial = 'c';
+    switch (initial) {
+    case 'j':
+    System.out.print("John ");
+    break;
+    case 'c':
+    System.out.print("Chuck ");
+    case 't':
+    System.out.print("Ty ");
+    break;
+    case 's':
+    System.out.print("Scott ");
+    default:
+    System.out.print("All");
+    }
+    }
+}    
