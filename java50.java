1993b143ffeb2349d677e5d6c33300ffe410e19c
k-rite <44746687+k-rite@users.noreply.github.com>
2022-10-11 01:14:12 +0530

    cvbn

---
+++ java50.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+class java50 {
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/mydata",)
+            
+        }
+    }
+}
---
+++ notes/doa/knapsack.py
@@ -0,0 +1,22 @@
+profit,weight,cap=[24,18,18,10],[24,10,10,7],25
+bag,value,calweight,c,calprofit=[],[],0,0,0
+for z in range(0,len(weight)):value.append(profit[z]/weight[z])
+priority= sorted(value, reverse=True)
+for i in range(len(weight)):
+    try:
+        if int(calweight+weight[c]) > cap:
+            c=value.index(priority[i])
+            ratio =  (cap -calweight) /weight[c]
+            calprofit -= profit[value.index(priority[i-1])]
+            calprofit+= profit[c]*ratio
+            print(f"We have taken the index {c} in ratio of {ratio} to fill the remaining space of {cap-calweight}")
+            break  
+        c=value.index(priority[i])   
+        calprofit+= profit[c]
+        bag.append(str(f"Index: {c}, Profit: {profit[c]}, Weight: {weight[c]}"))
+        calweight+=weight[c]
+    except:
+        print("We have filled everything, Remaining Space is:",cap-calweight)
+        break
+for i in bag:print(i)
+print("Total Profit:",calprofit)
